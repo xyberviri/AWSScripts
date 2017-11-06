c7fd61f109f9c8da97e30f3bd15f386fe48707ea
James Velasquez <xyberviri@gmail.com>
2017-11-06 10:07:08 -0500

    updated description and added timestap
    Added description to file purpose and timestamp to output log.

--- windows/batch/FileCleanup.cmd
+++ windows/batch/FileCleanup.cmd
@@ -1,4 +1,6 @@
-REM FTP clean up
+REM The purpose of this script is to move files older then %PURGE_AGE% from %PURGEPATH% to %PURGEDEST%\%PURGESTOR%
+REM and then delete files older then %DELET_AGE% from %PURGEDEST%\%PURGESTOR%
+REM a record of what was moved is kept in %PURGEDEST%\%PURGELOG%-%datetime%.log
 
 SET PURGEPATH=C:\Users\%username%\Downloads
 SET PURGEDEST=C:\Users\%username%\olddownloads
@@ -6,8 +8,14 @@ SET PURGE_AGE=35
 SET DELET_AGE=70
 
 SET PURGESTOR=storage
-SET PURGELOG=purgejob.log
+SET PURGELOG=purgejob
 SET DELFEMPT=Y
+
+FOR /f "tokens=1-8 delims=:./ " %%G IN ("%date%_%time%") DO (
+SET datetime=%%G%%H%%I_%%J_%%K
+)
+
+set PURGELOG=%PURGELOG%-%datetime%.log
 
 REM Robocopy files/folders older than PURGE_AGE
 robocopy "%PURGEPATH%" "%PURGEDEST%\%PURGESTOR%" *.* /move /copyall /s /MAXAGE:%PURGE_AGE% /LOG:"%PURGEDEST%\%PURGELOG%"
