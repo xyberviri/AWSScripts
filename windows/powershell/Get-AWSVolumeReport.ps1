c786154c227075917ca747ec5d46473a30567e5d
James Velasquez <xyberviri@gmail.com>
2020-07-08 18:39:58 -0400

    Update Get-AWSVolumeReport.ps1

--- windows/powershell/Get-AWSVolumeReport.ps1
+++ windows/powershell/Get-AWSVolumeReport.ps1
@@ -31,14 +31,12 @@ function Get-AWSVolumeReport
 {
 Param ([string]$region, [string]$awsProfile)
 write-host "Checking" $region
-
 $volsnaps=(Get-EC2Volume -Region $region -ProfileName $awsProfile) 
 $images = Get-EC2Image -Region $region -ProfileName $awsProfile -Owner (Get-STSCallerIdentity -Region $region -ProfileName $awsProfile).account
 $snapshots = Get-EC2Snapshot -Region $region -ProfileName $awsProfile
 $snapshotsWithImages = $snapshots  |? {$images.BlockDeviceMappings.Ebs.SnapshotId -contains $_.SnapshotId}
 $snapshotsWithoutImages = $snapshots  |? {$images.BlockDeviceMappings.Ebs.SnapshotId -notcontains $_.SnapshotId}
 $tag = [Amazon.EC2.Model.Tag]::new("Orphaned","True")
-
 $AllObjects = @()
 $count=0
 foreach($volume in $volsnaps) {
@@ -55,15 +53,12 @@ foreach($volume in $volsnaps) {
     }
     $count++
 }
-
 $AllObjects |Export-Csv -Path "$region-ec2volume-report.csv" -NoTypeInformation
 $snapshotsWithImages |Select-Object -Property SnapshotId,Description,StartTime,VolumeId,VolumeSize,Encrypted |export-csv -Path "$region-images.csv" -NoTypeInformation
-
-foreach($orphan in ($snapshotsWithoutImages |? {$_.Tag.Key -notcontains "Orphaned"})){
-write-host "Tagging" $orphan.SnapshotId
-New-EC2Tag -Resource $orphan.SnapshotId -Tag $tag -Region $region -ProfileName $awsProfile -ErrorAction SilentlyContinue
-}
-
+    foreach($orphan in ($snapshotsWithoutImages |? {$_.Tag.Key -notcontains "Orphaned"})){
+        write-host "Tagging" $orphan.SnapshotId
+        New-EC2Tag -Resource $orphan.SnapshotId -Tag $tag -Region $region -ProfileName $awsProfile -ErrorAction SilentlyContinue
+    }
 }
 
 Get-AWSVolumeReport -region "us-east-1" -profile "default"
